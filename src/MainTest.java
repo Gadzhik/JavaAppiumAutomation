67fe1372646b60040ef8cecff3babbab7602bb15
Gadzhik <gkurban@outlook.com>
2025-07-24 16:41:07 +0200

    Super и Override (переопределение)

--- src/MainTest.java
+++ src/MainTest.java
@@ -2,21 +2,19 @@ import org.junit.Test;
 
 public class MainTest extends CoreTestCase
 {
-    // Создаем объект класса MathHelper
-    MathHelper Math = new MathHelper();
-
-    // Переменные написанные вне функции - являются полями класса. Они видны в каждой функции класса. Мы пожем обратиться к полю класса через this.a
-    int a = 3;
-    int b = 12;
-
-    @Test
-    public void myFirstTest()
+    public void typeStartMessage()
     {
-        int a = Math.multiplay(3);
-        System.out.println(a);
-
-        int b = Math.multiplay(10, 17);
-        System.out.println(b);
+        super.typeStartMessage();
+        System.out.println("Current class is MainTest");
     }
 
+   @Test
+    public void myFirstTest() {
+        this.typeStartMessage();
+   }
+
+    @Test
+    public void mySecondTest() {
+        this.typeStartMessage();
+    }
 }
