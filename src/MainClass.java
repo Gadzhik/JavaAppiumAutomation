3976328f5342ade9789e932c15acfaced60b64f5
Gadzhik <gkurban@outlook.com>
2025-07-25 11:08:40 +0200

    Home work 2 - testGetClassNumber

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,6 +2,12 @@ import org.junit.Test;
 
 public class MainClass
 {
+    private int class_number = 20;
+
+    public int getClassNumber() {
+        return this.class_number;
+    }
+
     public int getLocalNumber(int number)
     {
         return number = 14;
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -3,11 +3,18 @@ import org.junit.Test;
 
 public class MainClassTest extends MainClass
 {
+    int number_get_class_n = this.getClassNumber();
+
     @Test
     public void testGetLocalNumber() {
         int expected = this.getLocalNumber(14);
         int actual = 2 * 7;
         Assert.assertTrue( "getLocalNumber not equal 14",actual == expected);
 
+    }
+
+    @Test
+    public void testGetClassNumber() {
+        Assert.assertTrue( "getClassNumber less then 45", number_get_class_n > 45);
     }
 }
