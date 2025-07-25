c34794d29cd76e3a88c5119ad1f9f8abb4053500
Gadzhik <gkurban@outlook.com>
2025-07-25 15:21:57 +0200

    Home work 3 - testGetClassString

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,7 +2,13 @@ import org.junit.Test;
 
 public class MainClass
 {
+    private String class_string = "Hello, world";
+
     private int class_number = 20;
+
+    public String getClassString() {
+        return this.class_string;
+    }
 
     public int getClassNumber() {
         return this.class_number;
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -5,6 +5,8 @@ public class MainClassTest extends MainClass
 {
     int number_get_class_n = this.getClassNumber();
 
+    String some_text = this.getClassString();
+
     @Test
     public void testGetLocalNumber() {
         int expected = this.getLocalNumber(14);
@@ -16,5 +18,10 @@ public class MainClassTest extends MainClass
     @Test
     public void testGetClassNumber() {
         Assert.assertTrue( "getClassNumber less then 45", number_get_class_n > 45);
+    }
+
+    @Test
+    public void testGetClassString() {
+        Assert.assertTrue("The line must contain Hello or hello",some_text.contains("Hello") || some_text.contains("hello"));
     }
 }
