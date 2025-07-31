96b45895d6061fcfd72eed7b6f98301717843b22
Gadzhik <gkurban@outlook.com>
2025-07-31 19:05:12 +0200

    Ex1: Конфигурация инструментов

---
+++ src/FirstTest.java
@@ -0,0 +1,45 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class FirstTest
+{
+    private AppiumDriver driver;
+    // Чтобы тест работал нам нужно добавить 3 метода. Это - setUp, tearDown, firstTest
+
+    // В методе Setup - мы будем устанавливать все необходимые параметры для того чтобы запустить Аппиум драйвер и поднять наше приложение на устройстве.
+    @Before
+    public void setUp() throws Exception
+    {
+        // задаем capabilities, которые необходимы Аппиуму для работы
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("platformVersion", "8.1.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "C:\\Users\\gadzhi\\IdeaProjects\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
+
+        // включаем андроид-драйвер и передаем ему все capabilities и путь из которого он должен запускаться
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+    // tearDown - драйвер будет выключаться
+    @After
+    public void tearDown()
+    {
+        driver.quit();
+    }
+    // firstTest - тут будет распологаться код теста
+    @Test
+    public void firstTest()
+    {
+        System.out.println("First test run");
+    }
+}
