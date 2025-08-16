f35afa9964ddf7eec204ecfcccfd7be7ef6db594
Gadzhik <gkurban@outlook.com>
2025-08-16 12:29:15 +0200

    Ex2: Создание метода - assertElementHasText

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,10 +1,12 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -50,7 +52,7 @@ public class FirstTest
                 "Cannot find 'Search wikipedia' input",
                 5
         );
-        waitForElementAndSendKey(
+        waitForElementAndSendKeys(
                 By.xpath("//*[contains(@text, 'Search…')]"),
                 "Java",
                 "Cannot find search input",
@@ -72,7 +74,7 @@ public class FirstTest
                 5
         );
 
-        waitForElementAndSendKey(
+        waitForElementAndSendKeys(
                 By.xpath("//*[contains(@text, 'Search…')]"),
                 "Java",
                 "Cannot find search input",
@@ -109,7 +111,7 @@ public class FirstTest
                 5
         );
 
-        waitForElementAndSendKey(
+        waitForElementAndSendKeys(
                 By.xpath("//*[contains(@text, 'Search…')]"),
                 "Java",
                 "Cannot find search input",
@@ -140,6 +142,86 @@ public class FirstTest
         );
     }
 
+    // добавляем тест для проверки свайпа
+    @Test
+    public void testSwipeArticle()
+    {
+        waitForElementAndClickSkip(
+                By.xpath("//*[contains(@text, 'Skip')]"),
+                "Cannot find 'Skip' button",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find 'Search wikipedia' input",
+                5
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Java",
+                "Cannot find search input",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title']//*[@text='Java (programming language)']"),
+                "Cannot find 'Search wikipedia for ElementAndClick' input",
+                15
+        );
+
+        waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find article title",
+                15
+        );
+
+        // добавляем метод для свайпа
+        swipeUp(2000);
+        swipeUp(2000);
+        swipeUp(2000);
+        swipeUp(2000);
+
+    }
+
+    @Test
+    public void testHasElement()
+    {
+        WebElement skipButton = driver.findElementByXPath("//*[contains(@text, 'SKIP')]");
+        skipButton.click();
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find Search Wikipedia",
+                5
+        );
+
+    }
+
+//    Ex2: Создание метода
+//        Необходимо написать функцию, которая проверяет наличие ожидаемого текста у элемента. Предлагается назвать ее assertElementHasText. На вход эта функция должна             принимать локатор элемент, ожидаемый текст и текст ошибки, который будет написан в случае, если элемент по этому локатору не содержит текст, который мы ожидаем.
+//        Также, необходимо написать тест, который проверяет, что поле ввода для поиска статьи содержит текст (в разных версиях приложения это могут быть тексты                    "Search..." или "Search Wikipedia", правильным вариантом следует считать тот, которые есть сейчас). Очевидно, что тест должен использовать написанный ранее             метод.
+//        Результат выполнения задания нужно отдельным коммитом положить в git. В качестве ответа прислать ссылку на коммит. Если вам потребовалось несколько коммитов для          выполнения одного задания - присылайте ссылки на все эти коммиты с комментариями.
+
+    // домашка по 3му модулю - Ex2: Создание метода
+    @Test
+    public void testSearchFieldText()
+    {
+        waitForElementAndClickSkip(
+                By.xpath("//*[contains(@text, 'Skip')]"),
+                "Cannot find 'Skip' button",
+                5
+        );
+
+        assertElementHasText(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia' text",
+                "Search Wikipedia",
+                5
+        );
+    }
+
     // отдельный метод для Wait, при помощи которого будем искать элемент по Xpath и ожидать его появления
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds)
     {
@@ -158,6 +240,14 @@ public class FirstTest
         return waitForElementPresent(by, error_message, 5);
     }
 
+    // for Skip button
+    private WebElement waitForElementAndClickSkip(By by, String error_message, long timeoutInSeconds)
+    {
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
     private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds)
     {
         WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
@@ -165,7 +255,7 @@ public class FirstTest
         return element;
     }
 
-    private WebElement waitForElementAndSendKey(By by, String value, String error_message, long timeoutInSeconds)
+    private WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeoutInSeconds)
     {
         WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
         element.sendKeys(value);
@@ -186,6 +276,35 @@ public class FirstTest
     {
         WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
         element.clear();
+        return element;
+    }
+
+    // пишем метод для свайпа снизу экрана вверх
+    protected void swipeUp(int timeOfSwipe)
+    {
+        TouchAction action = new TouchAction(driver);
+
+        // определяем размер экрана и получаем параметры девайса
+        Dimension size = driver.manage().window().getSize();
+
+        // задаем некоторые переменные. 1. Начальная перем по оси Х. 2. Конечная перем по оси Х. 3. Начальная перем по оси У. 4. Конечная перем по оси У. Т.к палец будет двигаться снизу вверх, то меняться будет только ось У.
+        int x = size.width / 2;
+        // получ нач точку, кот находится в 80% экрана, внизу
+        int start_y = (int) (size.height * 0.8);
+        int end_y = (int) (size.height * 0.2);
+
+        // добавляем действие - свайп. Нажимаем на экран, ждем некоторое время и перемещаем свайп вверх. Выбираем точку внизу экрана (где то по середине), после этого провести свайпом вверх экрана в точку которая находится в середине (по горизонтальной оси), после этого отпускаем "палец"
+        // perform() - отправляет всю указанную последовательность действий на выполнение
+        // press() - координаты для нажатия
+        // moveTo() - координаты для движения
+        action.press(x, start_y).waitAction(timeOfSwipe).moveTo(x, end_y).release().perform();
+    }
+
+    // домашка по 3му модулю - Ex2: Создание метода
+    private WebElement assertElementHasText(By by, String error_message, String text, long timeoutInSeconds)
+    {
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.getAttribute("text");
         return element;
     }
 
