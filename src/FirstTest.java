f5e33a4908a2a8851b5142465020572c40a81a59
Gadzhik <gkurban@outlook.com>
2025-08-21 15:59:45 +0200

    03. Save first article - overlay, swipe left, variableand 04. Swipe - debugging

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -7,12 +7,17 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
 
 public class FirstTest {
@@ -25,16 +30,28 @@ public class FirstTest {
         // задаем capabilities, которые необходимы Аппиуму для работы
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "AndroidTestDevice");
-        capabilities.setCapability("platformVersion", "8.1.0");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "C:\\Users\\gadzhi\\IdeaProjects\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
+        // for appium 1.5
+//        capabilities.setCapability("platformName", "Android");
+//        capabilities.setCapability("deviceName", "AndroidTestDevice");
+//        capabilities.setCapability("platformVersion", "8.1.0");
+////        capabilities.setCapability("automationName", "UiAutomator2");
+//        capabilities.setCapability("appPackage", "org.wikipedia");
+//        capabilities.setCapability("appActivity", ".main.MainActivity");
+//        capabilities.setCapability("app", "C:\\Users\\gadzhi\\IdeaProjects\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
+//
+//        // включаем андроид-драйвер и передаем ему все capabilities и путь из которого он должен запускаться
+//        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub/"), capabilities);
+
+        capabilities.setCapability("appium:platformName", "Android");
+        capabilities.setCapability("appium:deviceName", "emulator-5554");
+        capabilities.setCapability("appium:platformVersion", "8.1.0");
+        capabilities.setCapability("appium:automationName", "UiAutomator2");
+        capabilities.setCapability("appium:appPackage", "org.wikipedia");
+        capabilities.setCapability("appium:appActivity", ".main.MainActivity");
+        capabilities.setCapability("appium:app", "C:\\Users\\gadzhi\\IdeaProjects\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
 
         // включаем андроид-драйвер и передаем ему все capabilities и путь из которого он должен запускаться
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), capabilities);
     }
 
     // tearDown - драйвер будет выключаться
@@ -269,6 +286,114 @@ public class FirstTest {
         );
     }
 
+    // Module 4. 03. Save first article - overlay, swipe left, variable
+    @Test
+    public void saveFirstArticleToMyList()
+    {
+        waitForElementAndClickSkip(
+                By.xpath("//*[contains(@text, 'Skip')]"),
+                "Cannot find 'Skip' button",
+                5
+        );
+
+        // используем первые 4 метода для перехода к нужной статье
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find 'Search wikipedia' input",
+                5
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Java",
+                "Cannot find search input",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='Java (programming language)']"),
+                "Cannot find 'Search wikipedia' article",
+                5
+        );
+
+        // наверное стоит добавить нажатие на Header, потому, что не получается найти кнопку Got it в дереве ресурсов
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/view_page_header_image"),
+                "Cannot click to header image",
+                5
+        );
+
+        waitForElementPresent(
+                By.xpath("//android.view.View[@content-desc='Java (programming language)']"),
+                "Cannot find article title",
+                15
+        );
+
+        // TODO изменить на кнопку Save и далее по коду
+        waitForElementAndClick(
+            By.xpath("//android.widget.TextView[@content-desc='Save']"),
+                "Cannot find button to Save article",
+                5
+        );
+
+        // кликаем по пункту меню Add to list
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/snackbar_action"),
+                "Cannot find option 'Add to list'",
+                5
+        );
+
+        // очищаем поле с текстом
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/text_input"),
+                "Cannot find input to set name of articles folder",
+                5
+        );
+
+        // добавляем новый список для чтения
+        String name_of_folder = "Programming list";
+
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/text_input"),
+                name_of_folder,
+                "Cannot find 'Name of this list' field",
+                5
+        );
+
+        // click to OK button
+        waitForElementAndClick(
+                By.id("android:id/button1"),
+                "Cannot find 'OK' button",
+                5
+        );
+
+        // нажимаем кнопку View List
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/snackbar_action"),
+                "Cannot click to 'View List' button",
+                5
+        );
+
+//        waitForElementAndClick(
+//                By.id("org.wikipedia:id/page_list_item_title"),
+//                "Cannot click to 'View List' button",
+//                5
+//        );
+
+        swipeElementToLeft(
+                By.id("org.wikipedia:id/page_list_item_title"),
+                "Cannot find saved article"
+        );
+
+        // убеждаемся, что статья отсутствует
+        waitForElementNotPresent(
+                By.xpath("//*[@text='Java (programming language)']"),
+                "Cannot delete saved article",
+                5
+        );
+
+    }
+
    // отдельный метод для Wait, при помощи которого будем искать элемент по Xpath и ожидать его появления
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
@@ -326,10 +451,18 @@ public class FirstTest {
         TouchAction action = new TouchAction(driver);
 
         // определяем размер экрана и получаем параметры девайса
-        Dimension size = driver.manage().window().getSize();
+        Dimension size = driver
+                .manage()
+                .window()
+                .getSize();
 
-        // задаем некоторые переменные. 1. Начальная перем по оси Х. 2. Конечная перем по оси Х. 3. Начальная перем по оси У. 4. Конечная перем по оси У. Т.к палец будет двигаться снизу вверх, то меняться будет только ось У.
+        // задаем некоторые переменные.
+        // 1. Начальная перем по оси Х.
+        // 2. Конечная перем по оси Х.
+        // 3. Начальная перем по оси У.
+        // 4. Конечная перем по оси У. Т.к палец будет двигаться снизу вверх, то меняться будет только ось У.
         int x = size.width / 2;
+
         // получ нач точку, кот находится в 80% экрана, внизу
         int start_y = (int) (size.height * 0.8);
         int end_y = (int) (size.height * 0.2);
@@ -338,7 +471,12 @@ public class FirstTest {
         // perform() - отправляет всю указанную последовательность действий на выполнение
         // press() - координаты для нажатия
         // moveTo() - координаты для движения
-        action.press(x, start_y).waitAction(timeOfSwipe).moveTo(x, end_y).release().perform();
+        action
+                .press(x, start_y)
+                .waitAction(timeOfSwipe)
+                .moveTo(x, end_y)
+                .release()
+                .perform();
     }
 
     // Module 4. Lesson 02. Swipe till element found - swipeQuick, counter
@@ -364,6 +502,7 @@ public class FirstTest {
 
         // already_swiped - количество реальных свайпов
         int already_swiped = 0;
+
         while (driver.findElements(by).size() == 0)
         {
             if (already_swiped > max_swipes)
@@ -376,4 +515,93 @@ public class FirstTest {
             ++already_swiped;
         }
     }
+
+    // метод для удаления статьи при помощи свайпа - уже устарел. Новый метод находится прямо под старым.
+//    protected void swipeElementToLeft(By by, String error_message)
+//    {
+//        // находим элемент на странице
+//        WebElement element = waitForElementPresent(
+//                by,
+//                error_message,
+//                10);
+//
+//        // делаем свайп по элементу. Нужно обнаружить элемент, найти координаты по х и у и сдвинуть влево
+//
+//        // данная ф-я запишет в переменную самую левую точку нашего элемента
+//        int left_x = element.getLocation().getX();
+//
+//        int right_x = left_x + element.getSize().getWidth();
+//        int upper_y = element.getLocation().getY();
+//        int lower_y = upper_y + element.getSize().getHeight();
+//
+//        // находим середину элемента для свайпа
+//        int middle_y = (upper_y + lower_y) / 2;
+//
+//        TouchAction action = new TouchAction(driver);
+//
+//        action.press(right_x, middle_y);
+//        action.waitAction(400);
+//        action.moveTo(left_x, middle_y);
+//        action.release();
+//        action.perform();
+//    }
+
+    public void swipeElementToLeft(By by, String error_message) {
+
+        // Находим элемент на экране, ожидая его появления в течение 10 секунд.
+        WebElement element = waitForElementPresent(by, error_message, 10);
+
+        // Получаем координаты элемента на экране.
+        Point location = element.getLocation();
+        // Получаем размеры элемента (ширину и высоту).
+        Dimension size = element.getSize();
+
+        // Координата по оси X левой границы элемента.
+        int left_x = location.getX();
+        // Координата по оси X правой границы элемента.
+        int right_x = left_x + size.getWidth();
+        // Координата по оси Y верхней границы элемента.
+        int upper_y = location.getY();
+        // Координата по оси Y нижней границы элемента.
+        int lower_y = upper_y + size.getHeight();
+        // Координата по оси Y средней линии элемента.
+        int middle_y = upper_y + (size.getHeight() / 2);
+
+        // Начальная координата по оси X для свайпа (чуть левее правого края элемента).
+        int start_x = right_x - 20;
+        // Конечная координата по оси X для свайпа (чуть правее левого края элемента).
+        int end_x = left_x + 20;
+        // Начальная координата по оси Y для свайпа (по центру элемента).
+        int start_y = middle_y;
+        // Конечная координата по оси Y для свайпа (также по центру элемента).
+        int end_y = middle_y;
+
+        // Выполняем свайп с начальной точки до конечной с заданной продолжительностью.
+        this.swipe(
+                new Point(start_x, start_y),
+                new Point(end_x, end_y),
+                Duration.ofMillis(550)  // Устанавливаем продолжительность свайпа 550 миллисекунд.
+        );
+    }
+
+    protected void swipe(Point start, Point end, Duration duration) {
+
+        // Создаем объект, представляющий палец для выполнения свайпа.
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+        // Создаем последовательность действий для выполнения свайпа.
+        Sequence swipe = new Sequence(finger, 1);
+
+        // Добавляем действие для перемещения пальца к начальной точке.
+        swipe.addAction(finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), start.x, start.y));
+        // Добавляем действие для нажатия на экран в начальной точке.
+        swipe.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+        // Добавляем действие для перемещения пальца из начальной точки в конечную в течение заданного времени.
+        swipe.addAction(finger.createPointerMove(duration, PointerInput.Origin.viewport(), end.x, end.y));
+        // Добавляем действие для отпускания пальца от экрана в конечной точке.
+        swipe.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        // Выполняем последовательность действий (свайп).
+        this.driver.perform(Arrays.asList(swipe));
+    }
+
 }
