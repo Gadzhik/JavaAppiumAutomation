1d3b6d2b50bf77e65ead5b8c161561e8a5384eaf
Gadzhik <gkurban@outlook.com>
2025-08-31 15:44:09 +0200

    05. Переключение платформ

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -1,17 +1,16 @@
 package lib;
-
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import junit.framework.TestCase;
-import org.junit.After;
-import org.junit.Before;
 import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
 import java.net.URL;
 import java.time.Duration;
 
 public class CoreTestCase extends TestCase {
+
+    private static final String PLATFORM_IOS = "ios";
+    private static final String PLATFORM_ANDROID = "android";
 
     protected AppiumDriver driver;
     private static String AppiumURL = "http://127.0.0.1:4723";
@@ -26,18 +25,11 @@ public class CoreTestCase extends TestCase {
         super.setUp();
 
         // задаем capabilities, которые необходимы Аппиуму для работы
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        capabilities.setCapability("appium:platformName", "Android");
-        capabilities.setCapability("appium:deviceName", "emulator-5554");
-        capabilities.setCapability("appium:platformVersion", "8.1.0");
-        capabilities.setCapability("appium:automationName", "UiAutomator2");
-        capabilities.setCapability("appium:appPackage", "org.wikipedia");
-        capabilities.setCapability("appium:appActivity", ".main.MainActivity");
-        capabilities.setCapability("appium:app", "C:\\Users\\gadzhi\\IdeaProjects\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
+        DesiredCapabilities capabilities = this.getCapabilitiesByPlatformEnv();
 
         // включаем андроид-драйвер и передаем ему все capabilities и путь из которого он должен запускаться
         driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+
         // HW - Ex7*: Поворот экрана
         this.rotateScreenPortrait();
     }
@@ -69,5 +61,33 @@ public class CoreTestCase extends TestCase {
     protected void backgroundApp(int seconds)
     {
         driver.runAppInBackground(Duration.ofSeconds(2));
+    }
+
+    // метод для использования ENV VAR (PLATFORM=android)
+    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception
+    {
+        // записываем в переменную значение добавленой ENV VAR (PLATFORM=android)
+        String platform = System.getenv("PLATFORM");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        if (platform.equals(PLATFORM_ANDROID)) {
+            capabilities.setCapability("appium:platformName", "Android");
+            capabilities.setCapability("appium:deviceName", "emulator-5554");
+            capabilities.setCapability("appium:platformVersion", "8.1.0");
+            capabilities.setCapability("appium:automationName", "UiAutomator2");
+            capabilities.setCapability("appium:appPackage", "org.wikipedia");
+            capabilities.setCapability("appium:appActivity", ".main.MainActivity");
+            capabilities.setCapability("appium:app", "C:\\Users\\gadzhi\\IdeaProjects\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
+        } else if (platform.equals(PLATFORM_IOS)) {
+
+        capabilities.setCapability("appium:platformName", "iOS");
+        capabilities.setCapability("appium:deviceName", "iPhone SE");
+        capabilities.setCapability("appium:platformVersion", "11.3");
+        capabilities.setCapability("appium:app", "C:\\Users\\gadzhi\\IdeaProjects\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
+    } else {
+        throw new Exception("Cannot get run platform from env variable. Platform value " + platform);
+        }
+        return capabilities;
     }
 }
