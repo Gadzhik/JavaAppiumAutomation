e096834a2111dda9d1ab56b80c96cfad8fa180c3
Gadzhik <gkurban@outlook.com>
2025-09-06 13:41:52 +0200

    02. Рефакторинг тестов на поиск

--- src/lib/ui/SearchPageObject.java
+++ src/lib/ui/SearchPageObject.java
@@ -4,17 +4,17 @@ import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.By;
 
 // тут будут методы которые будут использоваться для поиска
-public class SearchPageObject extends MainPageObject
+abstract public class SearchPageObject extends MainPageObject
 {
     // задаем константы для xpath
-    private static final String
-            SKIP_BUTTON = "xpath://*[contains(@text, 'Skip')]",
-            SEARCH_INIT_ELEMENT = "xpath://*[contains(@text, 'Search Wikipedia')]",
-            SEARCH_INPUT = "xpath://*[contains(@text, 'Search Wikipedia')]",
-            SEARCH_CANCEL_BUTTON = "id:org.wikipedia:id/search_close_btn",
-            SEARCH_RESULT_BY_SUBSTRING_TPL = "xpath://*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='{SUBSTRING}']",
-            SEARCH_RESULT_ELEMENT = "xpath://*[@resource-id='org.wikipedia:id/search_results_list']//*[@resource-id='org.wikipedia:id/page_list_item_title']",
-            SEARCH_EMPTY_RESULT_ELEMENT = "xpath://*[@text='No results']";
+    protected static String
+            SKIP_BUTTON,
+            SEARCH_INIT_ELEMENT,
+            SEARCH_INPUT,
+            SEARCH_CANCEL_BUTTON,
+            SEARCH_RESULT_BY_SUBSTRING_TPL,
+            SEARCH_RESULT_ELEMENT,
+            SEARCH_EMPTY_RESULT_ELEMENT;
 
     // берем драйвер из MainPageObject
     public SearchPageObject(AppiumDriver driver)
---
+++ src/lib/ui/android/AndroidSearchPageObject.java
@@ -0,0 +1,22 @@
+package lib.ui.android;
+import io.appium.java_client.AppiumDriver;
+import lib.ui.SearchPageObject;
+
+public class AndroidSearchPageObject extends SearchPageObject
+{
+    // задаем константы для xpath
+    static {
+        SKIP_BUTTON = "xpath://*[contains(@text, 'Skip')]";
+                SEARCH_INIT_ELEMENT = "xpath://*[contains(@text, 'Search Wikipedia')]";
+                SEARCH_INPUT = "xpath://*[contains(@text, 'Search Wikipedia')]";
+                SEARCH_CANCEL_BUTTON = "id:org.wikipedia:id/search_close_btn";
+                SEARCH_RESULT_BY_SUBSTRING_TPL = "xpath://*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='{SUBSTRING}']";
+                SEARCH_RESULT_ELEMENT = "xpath://*[@resource-id='org.wikipedia:id/search_results_list']//*[@resource-id='org.wikipedia:id/page_list_item_title']";
+                SEARCH_EMPTY_RESULT_ELEMENT = "xpath://*[@text='No results']";
+    }
+
+    public AndroidSearchPageObject(AppiumDriver driver)
+    {
+        super(driver);
+    }
+}
